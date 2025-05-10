5f01efda96be94ef46baa62130397bcfcd931332
Vilander Costa <vilander.costa@gmail.com>
2025-05-09 22:28:52 -0300

    parte 01 - desafio

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
+/*function mediaNota(n1,n2,n3,n4){
+    let media = (n1+n2+n3+n4)/4
+    console.log("A média é: " + media.toFixed(2))
+}
 
-let n1=7, n2=5.5, n3=2,n4=4
+mediaNota(5,4,2,3.5)*/
+
+let n1=5,n2=7,n3=4,n4=3
 let media = (n1+n2+n3+n4)/4
 console.log("A média é: " + media.toFixed(2))
+
+
+if (media >= 5){
+    alert("Sua nota foi " + media +" Parabéns, você está aprovado(a)!" )
+}else{
+    alert("Sua nota foi " + media +" Você não atingiu a nota necessária. Está de recuperação!")
+}
+/*
+Vamos criar uma calculadora de média e aprender o que são variáveis e como realizar operações!
+Neste programa, focamos nas primeiras ferramentas principais de qualquer linguagem de programação: variáveis, operadores, arredondamento e console.
+Conteúdo DETALHADO
+Entender as diferenças entre HTML, CSS e JavaScript;
+Variáveis, manipulação dos valores armazenados e a memória do computador;
+Tipos de variáveis, como texto e inteiro;
+Fixando a quantidade de casas decimais com a função toFixed();
+
+Desafios
+Dependendo da nota, mostrar se a aluna ou aluno foi aprovada(o) ou não
+Alterar o fundo da tela da maneira que achar mais legal
+Imprimir na própria página o resultado, ao invés do console
+Criar um conversor de temperaturas entre farenheit e celcius
+Colocar a conta inteira da média em apenas uma linha
+Este capítulo trata sobre a sintaxe básica do JavaScript, declarações de variáveis, tipos de dados e literais.
+01-media.zip*/
+ 
