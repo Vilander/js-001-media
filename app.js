eb954546426135559f546b67b3f622fe68d11de8
Vilander <vilander.costa@gmail.com>
2025-05-12 16:00:03 -0300

    ajustes folha de estilo e js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+
+
 /*function mediaNota(n1,n2,n3,n4){
     let media = (n1+n2+n3+n4)/4
     console.log("A média é: " + media.toFixed(2))
 }
 
-mediaNota(5,4,2,3.5)*/
+mediaNota(5,4,2,3.5)
 
 let n1=5,n2=7,n3=4,n4=3
 let media = (n1+n2+n3+n4)/4
@@ -33,3 +35,59 @@ Colocar a conta inteira da média em apenas uma linha
 Este capítulo trata sobre a sintaxe básica do JavaScript, declarações de variáveis, tipos de dados e literais.
 01-media.zip*/
  
+const form = document.getElementById('formNotas');
+    const resultado = document.getElementById('resultado');
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      
+      let n1 = parseFloat(form.nota1.value);
+      let n2 = parseFloat(form.nota2.value);
+      let n3 = parseFloat(form.nota3.value);
+      let n4 = parseFloat(form.nota4.value);
+
+      
+      let media = ((n1 + n2 + n3 + n4) / 4).toFixed(2);
+
+      
+      let mensagem;
+        if (media >= 5) {
+         mensagem = `Sua nota foi ${media}. Parabéns, você está aprovado(a)!`;
+        } else {
+         mensagem = `Sua nota foi ${media}. Você não atingiu a nota necessária. Está de recuperação.`;
+        }
+
+      
+      resultado.textContent = mensagem;
+    });
+
+    const btnResetar = document.getElementById('btnResetar');
+
+    btnResetar.addEventListener('click', function () {
+  form.reset();               
+  resultado.textContent = ''; 
+});
+
+/*---------------*/
+
+function celsiusToFahrenheit(valNum) {
+  valNum = parseFloat(valNum);
+  if (!isNaN(valNum)) {
+    const fahrenheit = (valNum * 1.8) + 32;
+    document.getElementById("outputFahrenheit").innerHTML = fahrenheit.toFixed(2);
+  } else {
+    document.getElementById("outputFahrenheit").innerHTML = "";
+  }
+}
+
+function fahrenheitToCelsius(valNum) {
+  valNum = parseFloat(valNum);
+  if (!isNaN(valNum)) {
+    const celsius = (valNum - 32) / 1.8;
+    document.getElementById("outputCelsius").innerHTML = celsius.toFixed(2);
+  } else {
+    document.getElementById("outputCelsius").innerHTML = "";
+  }
+}
+
